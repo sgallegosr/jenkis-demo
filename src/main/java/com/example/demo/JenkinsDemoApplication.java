4ff6511fa29df986c3f06679d71f712c5645e5b7
roberto <sgallegosr@gmail.com>
2020-05-12 20:48:48 -0500

    actualizacion del archivo

--- src/main/java/com/example/demo/JenkinsDemoApplication.java
+++ src/main/java/com/example/demo/JenkinsDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JenkinsDemoApplication.class, args);
+		SpringApplication.run(JenkinsDemoApplication.class);
 	}
 
 }
